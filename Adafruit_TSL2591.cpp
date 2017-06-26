2cbb8cd8ee843c740d98dd7a6ab6178235b77711
Hamish Cunningham <hamish@gate.ac.uk>
2017-06-26 17:35:33 +0100

    add support for ESP32 (in the same way as for ESP8266)

--- Adafruit_TSL2591.cpp
+++ Adafruit_TSL2591.cpp
@@ -44,7 +44,7 @@
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 /**************************************************************************/
-#if defined(ESP8266)
+#if defined(ESP8266) || defined(ESP32)
 #include <pgmspace.h>
 #else
 #include <avr/pgmspace.h>
