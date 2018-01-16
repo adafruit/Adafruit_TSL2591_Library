13bc51e98216084c5d37c935d77a8daf731df19f
Travis CI <travis@travis-ci.org>
2018-01-16 06:31:27 +0000

    Deploy code docs to GitHub Pages Travis build: 2
    Commit: 0785bb4fd4495c9795778973049127d3e8c392fb

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['calculatelux',['calculateLux',['../class_adafruit___t_s_l2591.html#a17eaa9012d126bd3b65e2c8f7889d79f',1,'Adafruit_TSL2591']]],
+  ['calculatelux',['calculateLux',['../class_adafruit___t_s_l2591.html#a588d9c7bb3796beae907c2d6ac0540f5',1,'Adafruit_TSL2591']]],
   ['clearinterrupt',['clearInterrupt',['../class_adafruit___t_s_l2591.html#aa438d91917456b4e03e96c153c712282',1,'Adafruit_TSL2591']]]
 ];
