c0d7e7cece518aa72e262e44357f72130218822b
Doxygen CI <ci-arduino@invalid>
2021-02-10 15:41:08 +0000

    Deploy docs to GitHub Pages from commit 0419764373
    Commit: 0419764373add289b6dbb04796286d56c504170f
    GitHub Actions run: 555073132

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___t_s_l2591.html#a2c490a2fc9e40857a45490a189666359',1,'Adafruit_TSL2591::begin(TwoWire *theWire)'],['../class_adafruit___t_s_l2591.html#ace5e93c2eca9cdff9f79ea521cde6cd3',1,'Adafruit_TSL2591::begin()']]]
+  ['begin',['begin',['../class_adafruit___t_s_l2591.html#a6505cfde541782908345721eac2a4eeb',1,'Adafruit_TSL2591::begin(TwoWire *theWire, uint8_t addr=TSL2591_ADDR)'],['../class_adafruit___t_s_l2591.html#aaaf4c677c7d511c6e6f788026dd6db3a',1,'Adafruit_TSL2591::begin(uint8_t addr=TSL2591_ADDR)']]]
 ];
